0735c9d4e47b4a1556c126361dc3a0f46dba6ced
Austin Keate <mdrak2000@aol.com>
2021-12-16 17:08:32 -0600

    adding code

---
+++ FinalProject.js
@@ -0,0 +1,180 @@
+function WorkExperience(){
+    document.body.style.backgroundColor = 'thistle';
+    document.body.style.color = 'black';
+    document.querySelector('.changeable').innerHTML = `<section id="WorkExperience">
+    <h1>Work Experience</h1>
+    <br />
+
+    <h3>ERMC Aviation LLC (Summer 2021)</h3>
+    <p>Duties: Responsible for cleaning and maintaining the appearance of the department's facility 
+        and aircraft, collect trash, clean, scrub, vacuum, lavatories, and galleys, restock 
+        supplies throughout aircraft and facility floor, perform security searches on aircraft; 
+        inform the lead agent or supervisor of a security breach or unusual occurrence; follow 
+        all airport, company and airline security procedures, and other duties assigned
+    </p>
+    <p>About the Job: This was my first job in the aviation industry that allowed me to work in the airport 
+    and see first hand what happens in this work environment. It helped me to see all the regulations I study 
+    and rules that I was unaware of to make the practical connection between what I am learning and what the jobs
+    actually consist of. It also opened my eyes to some of the problems in the industry and what I think can be 
+    improved, if and when I get a chance. This will hopefully be my first of many jobs related to aviation. 
+    </p>
+    <br />
+
+    <h3>Tire Services Company (Summer 2019 and Summer 2020)</h3>
+    <p>Carefully inspected worn out truck tires, built retreaded tires with rubber and glue, 
+        inspected retreaded tires after they were reconstructed, stripped rusty truck rims, 
+        powder coated rims and painted them, and organized palettes of rims with forklift.
+    </p>
+    <p>About the Job: This is a family business where I am able to try and help beyond my job title. I enjoyed 
+    trying to solve problems on a bigger scale that helped the company in the short term, while also thinking 
+    about the future. This job has taught me many valuable skills, while also working with some great coworkers, 
+    where I hope to build a similar work environment for my future jobs. 
+    </p>
+    <br />
+
+    <h3>Two Men and a Truck (Summer 2019)</h3>
+    <p>Thoroughly wrapped house items in protective covering, carefully carried items in and 
+        out of the truck, and assisted customer with any other needs.
+    </p>
+    <p>About the Job: One of my shortest work experiences, but it taught me a lot being very close to management. 
+    The job that has required the most teamwork and allowed me to see first hand just how important it is for 
+    everyone to be on the same page. One key lesson that I learned in this job is that everyone must be working 
+    together and if they are not the team will fail. 
+    <p/>
+    <br />
+
+    <h3>Assistant Youth Coach (Spring 2014, Summer 2016, and Summer 2017)</h3>
+    <p>Demonstrated effective time management skills by directing drills and players during practice, 
+        instructed middle school-aged students on strategic soccer fundamentals, and enhanced personal 
+        leadership abilities by encouraging players, both on and off the field
+    </p>
+    <p>About the Job: This was very enjoyable in that I was able to share and teach something that I know a lot 
+    about to people that are trying to learn. In a scenario like this it was important for me to be patient and 
+    help as much as I can. After doing this, it allowed me to look back on my experience and see how far some of 
+    the players have come and that I was apart of their growth. I would definitely want to do this again if the 
+    opportunity presents itself. 
+    </p>
+    <br /> <br />
+    </section>`
+}
+
+function JobInformation(){
+    document.body.style.backgroundColor = 'rgb(175,161,136)';
+    document.body.style.color = 'black';
+    document.querySelector('.changeable').innerHTML = `<section id="Job Information"></section>
+    <h1>Job Information</h1>
+
+    <br />
+
+    <a href="https://www.linkedin.com/in/austin-keate-27b946206/"><img id="LinkedIn" src='images/linkedin.png'></a>
+    <br /> <br />
+    
+    <a href="mailto:keate2000@aol.com">Contact me through email (personal)</a>
+    <br /> <br />
+    <a href="mailto:austinkeate@lewisu.edu">Conact me through email (business)</a>`;
+}
+
+function Athletics(){
+    document.body.style.backgroundColor = 'rgb(161,231,171)';
+    document.body.style.color = 'black';
+    document.querySelector('.changeable').innerHTML = `<section id="Athletics"></section>
+    <h1>Atheltic History and Present</h1>
+    <div style="backkground-image: url('highschool.jpg');">
+    <h3>Andrew High School Soccer</h3>
+     <p>
+        Played on Andrew High School boys soccer from 2014-2017. On the varsity team 
+        for my sophomore, junior, and senior year being a regular starter. Won the regopnal
+        and sectional championship sophomore year, losing in the regional Final Junior year, 
+        was captain senior year along with being voted most valuable player. 
+    </p>
+    <li><a href="https://www.maxpreps.com/athlete/austin-keate/NbqC-k2rEeW-8KA2nzwbTA/default.htm">
+        My High School Soccer Stats</a></li>
+        <img id="hssoccer" src='images/highschool.jpg'>
+
+    <h3>Lewis University Men's Soccer</h3>
+    <p>
+        Played on the Lewis Men's Soccer team from 2018-2021. Did not play a game my frehsman 
+        year. Made 10 appearances during my sophomore and junior year being apart of a team that 
+        was third in one of the hardest DII conferences in the U.S. Became a regular starter senior 
+        year scoring one goal, tying the number 1 ranked team in the nation and making it to the NCAA 
+        tournament for the first time since 2013. 
+    </p>
+    <li><a href="https://lewisflyers.com/sports/mens-soccer/roster/austin-keate/9557"> My College Stats</a></li>
+    <img id="colsoccer" src='images/Lewis.png'>
+
+    <h3>Steel City Men's Soccer</h3>
+    <p>
+        Regular starter for Steel City Men's Soccer Club. A semiprofessional soccer team in the Southwest 
+        suburbs of Illinois that competes in the Midwest Premier league east division. Helped the team finish
+        in third for the inagural season, undefeated at home. Faced the professional team Chicago House who plays
+        in NISA. 
+    </p>
+    <li><a href="https://steelcityfc.org/">Steel City Men's Soccer</a></li>
+    <li><a href="https://midwestpl.com/">Midwest Premier League</a></li>
+    <img id="spsoccer" src='images/SteelCity.jpg'>
+    
+    <br /> <br />
+    `;
+    
+}
+
+function Education(){
+    document.body.style.backgroundColor = 'lightpink';
+    document.body.style.color = 'black';
+    document.querySelector('.changeable').innerHTML = `<section id="Education"></section>
+    <h1>Education</h1>
+    <h3>Victor J. Andrew High School</h3>
+    <p>
+        Graduated High in 2018, with a 3.7 GPA. Took 4 years of Spanish and Latin, receiving an award for taking 
+        multiple langauges. Was apart of the National Honors Society, Spanish National Honors Society, Mathletes, 
+        Latin Club, boys basketball, boys track, and captain of boys soccer as a scholar athlete. Received certifacations
+        in Microsoft Word, Excel, and Powerpoint. 
+    </p>
+    <img id="hs" src='images/andrew_hs.jpg'>
+
+    <h3>Lewis University</h3>
+    <p>
+        Attended Lewis Univeristy from 2018-2022. Graduating with a degree in Aviation Administration and a Minor in 
+        Air Traffic Control. Taken courses such as Private Pilot Ground, Instrument Pilot Ground, Aviation Meteorology, 
+        ATC Systems, Airport Management, Human Resource Management, Aviation Regulations, Principles of Accouting, and 
+        Fiscal Aspects of Transportation. These have set me up for a job in the aviation industry with a great variety of 
+        expertise and knowledge of the information. 
+    </p> 
+    <img id="col" src='images/lewis-campus.jfif'>
+    `;
+}
+
+function Interests(){
+    document.body.style.backgroundColor = 'lightgrey';
+    document.body.style.color = 'black';
+    document.querySelector('.changeable').innerHTML = `<section id="Interests"></section>
+    <h3>Soccer</h3>
+    <h4>Playing</h4>
+    <p>
+        I have been playing soccer since the age of 4 and have not found anything more fun to do. My dream was always to play 
+        professional, but as time went on I realized how unrealistic it is. I have enjoyed turning my experience and knowledge of 
+        the game into coaching others and playing at my right level. Growing up I have played for Chicago Fire Juniors academy, 
+        Eclipse, Chicago Magic Academy, and Oak Brook Soccer Club. I typically play defence because I have good lateral movement, 
+        am faster than most forwards, and it gives me more time to pass to teammates.
+    </p>
+    <h4>Watching</h4>
+    <p>
+        My favorite soccer team is Chelsea FC who play in the Premier League. When I was younger I went to London, England to their 
+        stadium and ever since then they have been my favorite. If their game is after the sun rise, I make it a priority to watch the 
+        game. 
+    </p>
+
+    <h3>Aviation</h3>
+    <p>
+        From a young age, I have always been interested in planes and as I got older, the entire industry intrigued me and I understood it 
+        well. I have been studying it for four years and worked in it for one. From this experince I know that it is always changing and based 
+        on the management I had when working, there is a lot of work for improvement and people to learn from. I hope to get a job at an airline 
+        rather than an airport because of the different focuses of each of the options. 
+    </p> `;
+}
+
+function Memz(){
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+    document.querySelector('.changeable').innerHTML = `<section id="Memes I Like">pogue's quizzes are great.</section> `;
+}
